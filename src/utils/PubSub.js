af8805a6779723268951db053e7152a67b8e0eb7
zhangweijian <34937436@qq.com>
2023-03-08 17:46:55 +0800

    'PubSub'

--- src/utils/PubSub.js
+++ src/utils/PubSub.js
@@ -1,21 +1,44 @@
-// export default PubSub=(function (root) {
-const PubSub = {
-  subscribeCallback: {},
-  subscribe(name, callback) {
-    if (!Array.isArray(this.subscribeCallback[name])) {
-      this.subscribeCallback[name] = [];
+// // export default PubSub=(function (root) {
+// const PubSub = {
+//   subscribeCallback: {},
+//   subscribe(name, callback) {
+//     if (!Array.isArray(this.subscribeCallback[name])) {
+//       this.subscribeCallback[name] = [];
+//     }
+//     this.subscribeCallback[name].push(callback);
+//   },
+//   publish(name, value) {
+//     this.subscribeCallback[name].forEach((item) => {
+//       item(value);
+//     });
+//   },
+// };
+
+// // return PubSub
+// // PubSub.__proto__.subscribeCallback = {};
+// export default PubSub;
+// // root.PubSub = PubSub;
+// // })(this);
+//--------------------------------------------------------
+export default (function () {
+  let subscribeCallback = {}
+  function subscribe(name, callback) {
+    if (!Array.isArray(subscribeCallback[name])) {
+      subscribeCallback[name] = [];
     }
-    this.subscribeCallback[name].push(callback);
-  },
-  publish(name, value) {
-    this.subscribeCallback[name].forEach((item) => {
+    subscribeCallback[name].push(callback);
+  }
+
+  function publish(name, value) {
+    subscribeCallback[name].forEach((item) => {
       item(value);
     });
-  },
-};
+  }
 
-// return PubSub
-// PubSub.__proto__.subscribeCallback = {};
-export default PubSub;
-// root.PubSub = PubSub;
-// })(this);
+  return {
+    subscribe,
+    publish
+  }
+})()
+
+
